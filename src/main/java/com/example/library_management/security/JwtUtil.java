c40332a1388d66500889eadd0efa79287846fa64
Ahmed Abd El-Samie <ahmed20shaheenuwk@gmail.com>
2025-03-27 01:29:23 +0200

    Adding JWT dependencis and JWT Util

---
+++ src/main/java/com/example/library_management/security/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.example.library_management.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private final String SECRET_KEY = "AsjkdsHFKJ743hkKDSLMCUisoJDSLKJ:FLK"; 
+    private final long EXPIRATION_TIME = 86400000;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+}
